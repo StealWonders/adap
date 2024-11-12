05635207bd9aad51c5a11c430871dfe7fd6ab05a
StealWonders <6114858+StealWonders@users.noreply.github.com>
2024-11-12 16:02:23 +0100

    more implementation

--- src/adap-b03/names/AbstractName.ts
+++ src/adap-b03/names/AbstractName.ts
@@ -1,6 +1,18 @@
-import { DEFAULT_DELIMITER, ESCAPE_CHARACTER } from "../common/Printable";
 import { Name } from "./Name";
+import { DEFAULT_DELIMITER, ESCAPE_CHARACTER } from "./../common/Printable";
 
+/**
+ * A name is a sequence of string components separated by a delimiter character.
+ * Special characters within the string may need masking, if they are to appear verbatim.
+ * There are only two special characters, the delimiter character and the escape character.
+ * The escape character can't be set, the delimiter character can.
+ * 
+ * Homogenous name examples
+ * 
+ * "oss.cs.fau.de" is a name with four name components and the delimiter character '.'.
+ * "///" is a name with four empty components and the delimiter character '/'.
+ * "Oh\.\.\." is a name with one component, if the delimiter character is '.'.
+ */
 export abstract class AbstractName implements Name {
 
     protected delimiter: string = DEFAULT_DELIMITER;
@@ -9,28 +21,51 @@ export abstract class AbstractName implements Name {
         this.delimiter = delimiter;
     }
 
-    public clone(): Name {
-        throw new Error("needs implementation");
-    }
-
+    // todo: escapted escape-characters are not handled correctly
+    // return a human-readable representation of the Name. If a component contains an escaped delimiter unescape it
     public asString(delimiter: string = this.delimiter): string {
-        throw new Error("needs implementation");
+        let result = "";
+        for (let i = 0; i < this.getNoComponents(); i++) {
+            result += this.getComponent(i);
+            if (i < this.getNoComponents() - 1) result += delimiter;
+        }
+        return result;
     }
 
-    public toString(): string {
-        throw new Error("needs implementation");
+    // todo: escapted escape-characters are not handled correctly
+    // return a human-readable representation of the Name. If a component contains an escaped delimiter unescape it
+    public toString(delimiter: string = this.delimiter): string {
+        let result = "";
+        for (let i = 0; i < this.getNoComponents(); i++) {
+            result += this.getComponent(i);
+            if (i < this.getNoComponents() - 1) result += delimiter;
+        }
+        return result;
     }
 
+    // todo: escapted escape-characters are not handled correctly
+    // join the components with the default delimiter so that the Name can be parsed back in from the string
     public asDataString(): string {
-        throw new Error("needs implementation");
+        let result = "";
+        for (let i = 0; i < this.getNoComponents(); i++) {
+            result += this.getComponent(i)
+                .replaceAll(ESCAPE_CHARACTER + this.delimiter, this.delimiter)
+                .replaceAll(ESCAPE_CHARACTER + ESCAPE_CHARACTER, ESCAPE_CHARACTER);
+            if (i < this.getNoComponents() - 1) result += DEFAULT_DELIMITER;
+        }
+        return result;
     }
 
     public isEqual(other: Name): boolean {
-        throw new Error("needs implementation");
+        return this.isEqual(other);
     }
 
     public getHashCode(): number {
-        throw new Error("needs implementation");
+        return this.getHashCode();
+    }
+
+    public clone(): Name {
+        return Object.create(this);
     }
 
     public isEmpty(): boolean {
@@ -51,7 +86,10 @@ export abstract class AbstractName implements Name {
     abstract remove(i: number): void;
 
     public concat(other: Name): void {
-        throw new Error("needs implementation");
+        if (this.delimiter !== other.getDelimiterCharacter()) throw new Error("delimiters do not match");
+        for (let i = 0; i < other.getNoComponents(); i++) {
+            this.append(other.getComponent(i));
+        }
     }
 
     protected checkBounds(i: number): void {
