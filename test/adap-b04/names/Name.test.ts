31aca8dc1c49ca844d0c9022cbea719e7263fa56
StealWonders <6114858+StealWonders@users.noreply.github.com>
2024-11-24 14:36:02 +0100

    copy tests from b03->b04

---
+++ test/adap-b04/names/Name.test.ts
@@ -0,0 +1,59 @@
+import { describe, it, expect } from "vitest";
+import { StringName } from "../../../src/adap-b04/names/StringName";
+import { StringArrayName } from "../../../src/adap-b04/names/StringArrayName";
+import { AbstractName } from "../../../src/adap-b04/names/AbstractName";
+
+
+describe("Basic StringName function tests", () => {
+  it("test insert", () => {
+    let n: AbstractName = new StringName("oss.fau.de");
+    n.insert(1, "cs");
+    expect(n.asString()).toBe("oss.cs.fau.de");
+  });
+  it("test append", () => {
+    let n: AbstractName = new StringName("oss.cs.fau");
+    n.append("de");
+    expect(n.asString()).toBe("oss.cs.fau.de");
+  });
+  it("test remove", () => {
+    let n: AbstractName = new StringName("oss.cs.fau.de");
+    n.remove(0);
+    expect(n.asString()).toBe("cs.fau.de");
+  });
+});
+
+describe("Basic StringArrayName function tests", () => {
+  it("test insert", () => {
+    let n: AbstractName = new StringArrayName(["oss", "fau", "de"]);
+    n.insert(1, "cs");
+    expect(n.asString()).toBe("oss.cs.fau.de");
+  });
+  it("test append", () => {
+    let n: AbstractName = new StringArrayName(["oss", "cs", "fau"]);
+    n.append("de");
+    expect(n.asString()).toBe("oss.cs.fau.de");
+  });
+  it("test remove", () => {
+    let n: AbstractName = new StringArrayName(["oss", "cs", "fau", "de"]);
+    n.remove(0);
+    expect(n.asString()).toBe("cs.fau.de");
+  });
+});
+
+describe("Delimiter function tests", () => {
+  it("test insert", () => {
+    let n: AbstractName = new StringName("oss#fau#de", '#');
+    n.insert(1, "cs");
+    expect(n.asString()).toBe("oss#cs#fau#de");
+  });
+});
+
+describe("Escape character extravaganza", () => {
+  it("test escape and delimiter boundary conditions", () => {
+    let n: AbstractName = new StringName("oss.cs.fau.de", '#');
+    expect(n.getNoComponents()).toBe(1);
+    expect(n.asString()).toBe("oss.cs.fau.de");
+    n.append("people");
+    expect(n.asString()).toBe("oss.cs.fau.de#people");
+  });
+});
