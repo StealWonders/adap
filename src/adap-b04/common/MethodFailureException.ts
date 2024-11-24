839b14d853d43e31bd0745fe70d7c1050c2138f1
StealWonders <6114858+StealWonders@users.noreply.github.com>
2024-11-24 18:19:25 +0100

    bing chilling

--- src/adap-b04/common/MethodFailureException.ts
+++ src/adap-b04/common/MethodFailureException.ts
@@ -6,12 +6,22 @@ import { Exception } from "./Exception";
  */
 export class MethodFailureException extends Exception {
 
-    static assertIsNotNullOrUndefined(o: Object | null, exMsg: string = "null or undefined"): void {
+    static assertIsNotNullOrUndefined(o: any, exMsg: string = "null or undefined"): void {
         this.assertCondition(!this.isNullOrUndefined(o), exMsg);
     }
     
     static assertCondition(cond: boolean, exMsg: string): void {
         if (!cond) throw new MethodFailureException(exMsg);
+    }
+
+    static assertIsNotNullOrUndefinedWithCallback(callback: () => void, o: any, exMsg: string = "null or undefined"): void {
+        this.assertConditionWithCallback(callback, !this.isNullOrUndefined(o), exMsg);
+    }
+
+    static assertConditionWithCallback(callback: () => void, cond: boolean, exMsg: string): void {
+        if (cond) return;
+        callback();
+        throw new MethodFailureException(exMsg);
     }
 
     constructor(m: string) {
